02395bfe93709329c3690ff6d7b7840cbf5b5cc7
clannad <1945534669@qq.com>
2023-03-24 21:47:13 +0800

    updata

--- 01-Python 万能代码模版：10 大必学实用技巧/1.1 巧用 Python 爬虫，实现财富自由/Crawler.py
+++ 01-Python 万能代码模版：10 大必学实用技巧/1.1 巧用 Python 爬虫，实现财富自由/Crawler.py
@@ -18,12 +18,14 @@ def save_to_file(filename, content):
     with open(filename, mode="w", encoding="utf-8") as f:
         f.write(content)
 
+
 def create_doc_from_filename(filename):
     # 输入参数为要分析的 html 文件名，返回值为对应的 BeautifulSoup 对象
     with open(filename, "r", encoding='utf-8') as f:
         html_content = f.read()
         soup = BeautifulSoup(html_content, "lxml")
     return soup
+
 
 def parse(soup):
     post_list = soup.find_all("div", class_="post-info")
@@ -41,6 +43,7 @@ def main():
     save_to_file(filename, result)
     soup = create_doc_from_filename(filename)
     parse(soup)
+
 
 if __name__ == '__main__':
     main()
