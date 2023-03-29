5f940a9538606a82ef04d470d1592d4663f84984
clannad <1945534669@qq.com>
2023-03-29 21:47:41 +0800

    updata

--- Spider_Test/spider_test.py
+++ Spider_Test/spider_test.py
@@ -1,46 +1,52 @@
 # -*- coding = utf-8 -*-
 
-import re   # 正则表达式，进行文字匹配
+import re  # 正则表达式，进行文字匹配
+import urllib.error  # 制定URL，获取网页数据
 import urllib.request
-import urllib.error     # 制定URL，获取网页数据
-import xlwt     # 进行excel操作
-
-from bs4 import BeautifulSoup       # 网页解析，获取数据
-from lxml import etree
+import xlwt  # 进行excel操作
 # import sqlite3    # 进行SQLite数据库操作
 
-findLink = re.compile(r'<a href="(.*?)">')      # 创建正则表达式对象，标售规则   影片详情链接的规则
-findImgSrc = re.compile(r'<img.*src="(.*?)"', re.S)
-findTitle = re.compile(r'<span class="title">(.*)</span>')
-findRating = re.compile(r'<span class="rating_num" property="v:average">(.*)</span>')
-findJudge = re.compile(r'<span>(\d*)人评价</span>')
-findInq = re.compile(r'<span class="inq">(.*)</span>')
-findBd = re.compile(r'<p class="">(.*?)</p>', re.S)
+from bs4 import BeautifulSoup  # 网页解析，获取数据
+from lxml import etree
+
+"""创建正则表达式对象"""
+# pattern_Link = re.compile(r'//div[@class="hd"]/a')      # url链接
+# pattern_ImgSrc = re.compile(r'<img.*src="(.*?)"', re.S)     # 配图链接//div[@class="pic"]/a/img
+pattern_Title = re.compile(r'//div[@class="hd"]/a/span[1]')     # 电影中文名
+pattern_Rating = re.compile(r'//span[@class="rating_num"]')    # 电影评分
+pattern_Judge = re.compile(r'//div[@class="star"]/span[4]')    # 点评人数
+pattern_Inq = re.compile(r'//span[@class="inq"]')   # 概况
+pattern_Bd = re.compile(r'//div[@class="bd"]/p[1]')     # 相关信息
 
 
 def main():
-    baseurl = "https://movie.douban.com/top250?start="      # 要爬取的网页链接
-    # 1.爬取网页
+    """
+    baseurl: 要爬取的网页链接
+    savepath: 当前目录新建XLS，存储进去
+    dbpath: 当前目录新建数据库，存储进去
+    """
+    baseurl = "https://movie.douban.com/top250?start="
+    """爬取网页"""
     datalist = getData(baseurl)
-    savepath = "豆瓣电影Top250.xls"     # 当前目录新建XLS，存储进去
-    # dbpath = "movie.db"              #当前目录新建数据库，存储进去
-    # 3.保存数据
-    saveData(datalist, savepath)    # 2种存储方式可以只选择一种
+    savepath = "豆瓣电影Top250.xls"
+    # dbpath = "movie.db"
+    """保存数据,2种存储方式可以只选择一种"""
+    saveData(datalist, savepath)
     # saveData2DB(datalist,dbpath)
 
 
-# 爬取网页
 def getData(baseurl):
-    datalist = []   # 用来存储爬取的网页信息
-    for i in range(0, 10):      # 调用获取页面信息的函数，10次
+    """爬取网页"""
+    datalist = []  # 用来存储爬取的网页信息
+    for i in range(0, 10):  # 调用获取页面信息的函数，10次
         url = baseurl + str(i * 25)
-        html = askURL(url)      # 保存获取到的网页源码
+        html = askURL(url)  # 保存获取到的网页源码
         # 2.逐一解析数据
-        soup = BeautifulSoup(html, "html.parser")
-        for item in soup.find_all('div', class_="item"):     # 查找符合要求的字符串
+        tree = etree.HTML(html)
+        for item in tree.findall('div', class_="item"):  # 查找符合要求的字符串
             data = []  # 保存一部电影所有信息
             item = str(item)
-            link = re.findall(findLink, item)[0]    # 通过正则表达式查找
+            link = re.findall(findLink, item)[0]  # 通过正则表达式查找
             data.append(link)
             imgSrc = re.findall(findImgSrc, item)[0]
             data.append(imgSrc)
@@ -48,7 +54,7 @@ def getData(baseurl):
             if (len(titles) == 2):
                 ctitle = titles[0]
                 data.append(ctitle)
-                otitle = titles[1].replace("/", "")     # 消除转义字符
+                otitle = titles[1].replace("/", "")  # 消除转义字符
                 data.append(otitle)
             else:
                 data.append(titles[0])
@@ -72,9 +78,11 @@ def getData(baseurl):
     return datalist
 
 
-"""得到指定一个URL的网页内容"""
 def askURL(url):
-    head = {    # 模拟浏览器头部信息，向豆瓣服务器发送消息
+    """得到指定一个URL的网页内容"""
+
+    """模拟浏览器头部信息，向豆瓣服务器发送消息"""
+    head = {
         "User-Agent": "Mozilla / 5.0(Windows NT 10.0; Win64; x64) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 80.0.3987.122  Safari / 537.36"
     }
     # 用户代理，表示告诉豆瓣服务器，我们是什么类型的机器、浏览器（本质上是告诉浏览器，我们可以接收什么水平的文件内容）
@@ -92,20 +100,21 @@ def askURL(url):
     return html
 
 
-# 保存数据到表格
 def saveData(datalist, savepath):
+    """保存数据到表格"""
+
     print("save.......")
-    book = xlwt.Workbook(encoding="utf-8", style_compression=0)     # 创建workbook对象
-    sheet = book.add_sheet('豆瓣电影Top250', cell_overwrite_ok=True)    # 创建工作表
+    book = xlwt.Workbook(encoding="utf-8", style_compression=0)  # 创建workbook对象
+    sheet = book.add_sheet('豆瓣电影Top250', cell_overwrite_ok=True)  # 创建工作表
     col = ("电影详情链接", "图片链接", "影片中文名", "影片外国名", "评分", "评价数", "概况", "相关信息")
     for i in range(0, 8):
-        sheet.write(0, i, col[i])   # 列名
+        sheet.write(0, i, col[i])  # 列名
     for i in range(0, 250):
         # print("第%d条" %(i+1))       #输出语句，用来测试
         data = datalist[i]
         for j in range(0, 8):
-            sheet.write(i + 1, j, data[j])      # 数据
-    book.save(savepath)     # 保存
+            sheet.write(i + 1, j, data[j])  # 数据
+    book.save(savepath)  # 保存
 
 
 # def saveData2DB(datalist,dbpath):
@@ -153,8 +162,8 @@ def saveData(datalist, savepath):
 # 保存数据到数据库
 
 
-if __name__ == "__main__":      # 当程序执行时
-    # 调用函数
+if __name__ == "__main__":  # 当程序执行时
+    """调用函数"""
     main()
     # init_db("movietest.db")
     print("爬取完毕！")
